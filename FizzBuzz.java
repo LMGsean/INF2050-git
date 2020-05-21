35442fff80d94031d27c5917dbea04877b3447e2
Sean <CArmsprod@gmail.com>
2020-05-21 11:53:37 -0400

    ajout .swp

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz{
 	public static void main (String[] args){
 		
 		//un genre de if
-		int max = (args.length >0) ? Integer.parseInt(args[0]): 200;
+		int max = (args.length >0) ? Integer.parseInt(args[0]): 100;
 		
 		for(int i=1; i<=max; ++i){
 			if(i % 15 == 0){System.out.println("FizzBuzz");}
