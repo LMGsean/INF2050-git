94bf1bcaa67141be65316105785430c09891384b
Sean <CArmsprod@gmail.com>
2020-05-21 11:14:09 -0400

    ajout ignore file

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz{
+	public static void main (String[] args){
+		
+		//un genre de if
+		int max = (args.length >0) ? Integer.parseInt(args[0]): 100;
+		
+		for(int i=1; i<=max; ++i){
+			if(i % 15 == 0){System.out.println("FizzBuzz");}
+			else if(i % 3 == 0){System.out.println("Fizz");}
+			else{System.out.println(i);}
+		}	
+	}
+}
+
